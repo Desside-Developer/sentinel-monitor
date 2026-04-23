4ea3a93dc7d47808716770a651ac70ec4ccba5db
Desside-Developer <desside.programming@gmail.com>
2026-04-23 15:22:13 +0300

    [ Add Spring Boot 4.0.5 ] Setup sentinel-monitor

---
+++ src/test/java/com/desside/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desside.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
