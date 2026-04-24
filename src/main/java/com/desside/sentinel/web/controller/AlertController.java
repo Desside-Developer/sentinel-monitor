c9b7fa835afcf688ce740e7aa915f345ffa9c8b8
Desside-Developer <desside.programming@gmail.com>
2026-04-24 14:59:42 +0300

    [ Dependencies ] add implementation 'org.springframework.boot:spring-boot-starter-actuator'

--- src/main/java/com/desside/sentinel/web/controller/AlertController.java
+++ src/main/java/com/desside/sentinel/web/controller/AlertController.java
@@ -1,4 +1,19 @@
 package com.desside.sentinel.web.controller;
 
+import com.desside.sentinel.domain.Alert;
+import com.desside.sentinel.service.AlertService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alerts") @RequiredArgsConstructor
 public class AlertController {
+    private final AlertService alertService;
+
+    @GetMapping
+    public List<Alert> getRecent() { return alertService.getRecent(); }
 }
--- src/main/java/com/desside/sentinel/web/controller/StatusController.java
+++ src/main/java/com/desside/sentinel/web/controller/StatusController.java
@@ -1,4 +1,21 @@
 package com.desside.sentinel.web.controller;
 
+import com.desside.sentinel.service.StatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/status") @RequiredArgsConstructor
 public class StatusController {
+    private final StatusService statusService;
+
+    @GetMapping("/{serviceId}")
+    public Map<String, String> getStatus(@PathVariable String serviceId) {
+        return Map.of("serviceId", serviceId, "status", statusService.getStatus(serviceId));
+    }
 }
