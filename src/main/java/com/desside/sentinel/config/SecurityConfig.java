a630ca535624ba9a2ad37b8309e422f16cd74bcb
Desside-Developer <desside.programming@gmail.com>
2026-04-24 15:47:32 +0300

    create access to "/actuator/**"

--- src/main/java/com/desside/sentinel/config/SecurityConfig.java
+++ src/main/java/com/desside/sentinel/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.desside.sentinel.config;
 
 import com.desside.sentinel.security.JwtFilter;
+import com.desside.sentinel.security.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,7 +30,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(s -> s.sessionCreationPolicy(STATELESS))
                 .authorizeHttpRequests(a -> a
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**", "/actuator/**").permitAll() //.requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
